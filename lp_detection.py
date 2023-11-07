4874700db95c3df3a0e5605806f6d6ebca07dcd2
Minjule <minjinsor.minjee0909@gmail.com>
2023-11-07 11:31:14 +0800

    second attempt

--- lp_detection.py
+++ lp_detection.py
@@ -2,4 +2,8 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 
-results = model.train(data='data.yaml', epochs=3, imgsz=640)
+def main():
+    results = model.train(data='data.yaml', epochs=3, imgsz=640, batch = 4)
+
+if __name__ == '__main__':
+    main()
